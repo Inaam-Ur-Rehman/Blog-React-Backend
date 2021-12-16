9465d3f08a44b66ec4fdc1934d7c4ae90ef16d68
Muhammad Inaam Ur Rehman <inaamurrehman343@gmail.com>
2021-12-16 23:11:49 +0500

    secnff

---
+++ Desktop/Web 2/daily-blog/src/components/Description/Info.js
@@ -0,0 +1,11 @@
+import "./Description.css";
+
+const Info = ({ description }) => {
+    return (
+        <div className="description__info">
+            {description}
+        </div>
+    )
+}
+
+export default Info
---
+++ Desktop/Web 2/daily-blog/src/pages/Description.js
@@ -0,0 +1,38 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Header from "../components/Description/Header";
+import Info from "../components/Description/Info";
+const axios = require('axios').default;
+const Description = () => {
+
+    const { blogId } = useParams();
+    const [blog, setBlog] = useState([]);
+    useEffect(() => {
+
+        async function getAllBlogs() {
+
+            axios.get(`http://localhost:8000/blog/${blogId}`).then((res) => {
+                if (res.status === 200) {
+                    setBlog(res.data);
+                }
+                else if (res.status === 400) {
+                    console.log("Not found");
+                }
+            }).catch((e) => {
+                console.log("ID is badlly formated")
+            })
+        }
+        getAllBlogs();
+
+    }, [blogId])
+    document.title = blog.title;
+    return (
+
+        <div>
+            <Header title={blog && blog.title} />
+            <Info description={blog && blog.Description} />
+        </div>
+    )
+}
+
+export default Description
